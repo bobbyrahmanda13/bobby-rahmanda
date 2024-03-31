31306f0058f7330f8c6b397e7a96d17dbd08731d
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-31 10:40:05 +0700

    chore: config page directory and layout first

--- app.vue
+++ app.vue
@@ -1,14 +1,11 @@
-<script setup lang="ts">
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
 
+<script setup lang="ts">
 import '@unocss/reset/tailwind.css'
 import './styles/base.css'
 
 </script>
-
-<template>
-  <div>
-    <!-- <NuxtWelcome /> -->
-    <h1 class="text-3xl text-blue-700">hello world</h1>
-  </div>
-</template>
-
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@vueuse/nuxt",
-    "@unocss/nuxt"
-  ]
+  modules: ["@vueuse/nuxt", "@unocss/nuxt", "@nuxt/content"],
+
 })
