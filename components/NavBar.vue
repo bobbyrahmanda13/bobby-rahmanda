fd66629a649a4485d7ead78e96094b5cc6f33bb8
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-03-31 12:53:35 +0700

    chore: progress NavBar

---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <header class="sticky top0">
+    <div class="bg-green-500 text-black">
+      <nav>
+        <NuxtLink to="/" class="">Home</NuxtLink>
+
+        <NuxtLink to="/" class="">Blog</NuxtLink>
+
+        <NuxtLink to="/" class="">Project</NuxtLink>
+
+        <NuxtLink to="/" class="">About</NuxtLink>
+      </nav>
+    </div>
+  </header>
+
+</template
