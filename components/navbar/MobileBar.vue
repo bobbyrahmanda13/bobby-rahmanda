7964f5be7be38c3b053d2d8afd9064d108238b9d
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-04-12 11:15:35 +0700

    chore: change utility class to native css scope vue

--- components/navbar/MobileBar.vue
+++ components/navbar/MobileBar.vue
@@ -1,40 +1,64 @@
 <template>
 
-    <header
-      class="md:hidden w-100% fixed z-1000 px-12 py-1rem me-auto m-bs-0 m-be-0 flex justify-between items-center position-bottom-none backdrop-blur-md">
+  <header
+    class="header-class">
 
-      <NuxtLink to="/" class="fw-bold list-none decoration-none font-size-none text-colorText hover:text-colorTwo">
-        Rahman
-      </NuxtLink>
+    <NuxtLink to="/" class="fw-bold list-none decoration-none font-size-none text-colorText hover:text-colorTwo">
+      Rahman
+    </NuxtLink>
 
-      <nav>
+    <nav>
 
-        <ul class="flex gap-2rem list-none items-center">
+      <ul class="flex gap-2rem list-none items-center">
 
-          <li>
-            <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">Home</NuxtLink>
-          </li>
+        <li>
+          <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">Home</NuxtLink>
+        </li>
 
-          <li>
-            <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">Blog</NuxtLink>
-          </li>
+        <li>
+          <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">Blog</NuxtLink>
+        </li>
 
-          <li>
-            <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">About</NuxtLink>
-          </li>
+        <li>
+          <NuxtLink to="/" class="text-colorText hover:text-colorTwo" active-class="text-colorTwo">About</NuxtLink>
+        </li>
 
-          <li class="flex justify-between items-center">
-            <NuxtLink to="https://github.com/bobbyrahmanda13"
-              class="mx-0.5rem text-lg text-colorText hover:text-colorTwo">
-              <div class="i-bi-github" />
-            </NuxtLink>
-            <NuxtLink to="https://t.me/Rahman_0000" class="mx-0.5rem text-lg text-colorText hover:text-colorTwo">
-              <div class="i-logos-telegram" />
-            </NuxtLink>
-          </li>
+        <li class="flex justify-between items-center">
+          <NuxtLink to="https://github.com/bobbyrahmanda13"
+            class="mx-0.5rem text-lg text-colorText hover:text-colorTwo">
+            <div class="i-bi-github" />
+          </NuxtLink>
+          <NuxtLink to="https://t.me/Rahman_0000" class="mx-0.5rem text-lg text-colorText hover:text-colorTwo">
+            <div class="i-logos-telegram" />
+          </NuxtLink>
+        </li>
 
-        </ul>
-      </nav>
-    </header>
+      </ul>
+    </nav>
+  </header>
 
 </template>
+<style scoped>
+.header-class header {
+
+  width: 100%;
+  z-index: 1000;
+  position: fixed;
+  padding: 1rem 3rem;
+  margin-inline: auto;
+  margin-block: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  bottom: 0;
+  --backdrop-blur: blur(12px);
+  -webkit-backdrop-filter: var(--backdrop-blur);
+  backdrop-filter: var(--backdrop-blur);
+}
+
+@media screen and (width >=768px) {
+  .header-class header {
+    display: hidden;
+  }
+}
+</style>
