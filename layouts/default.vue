2618a7a6598076e50f60319574f033eed280440f
bobbyrahmanda13 <bobbyrahmanda1996@protonmail.com>
2024-04-20 17:28:02 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 
   <NavBar />
 
-  <main>
+  <main class="container">
     <slot />
   </main>
 
@@ -10,4 +10,40 @@
   <!-- </footer> -->
 </template>
 
+<style scoped>
 
+.container {
+  width: 100%;
+  margin: 1rem auto;
+}
+
+@media (width >= 640px) {
+  .container {
+    max-width: 640px;
+  }
+}
+
+@media (width >= 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+
+@media (width >= 1024px) {
+  .container {
+    max-width: 1024px;
+  }
+}
+
+@media (width >= 1280px) {
+  .container {
+    max-width: 1280px;
+  }
+}
+
+@media (width >= 1536px) {
+  .container {
+    max-width: 1536px;
+  }
+}
+</style>
