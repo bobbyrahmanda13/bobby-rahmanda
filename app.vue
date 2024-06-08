2b699d3bbf4a13b1f5c352c46021729d7f154a1f
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-08 19:40:52 +0700

    update indent

--- app.vue
+++ app.vue
@@ -1,13 +1,12 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
-  </NuxtLayout>
+  </NuxtLayout
 </template>
 
 <script setup lang="ts">
 import '@unocss/reset/tailwind.css'
 import './styles/base.css'
-
 
 </script>
 
