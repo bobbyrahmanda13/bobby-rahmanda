2bd012fa5d7efa6de560dc4685c1b051c629858a
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-09 19:37:25 +0700

    look color pallete config unocss

--- components/navbar/DesktopBar.vue
+++ components/navbar/DesktopBar.vue
@@ -3,7 +3,7 @@
     <header
       class="hidden sticky z-1000 w-100% px-12 py-1rem me-auto m-be-0 m-bs-0 md:flex justify-between items-center position-top-none backdrop-blur-md">
 
-      <NuxtLink to="/" class="fw-bold list-none decoration-none font-size-none text-white hover:text-colorTwo">
+      <NuxtLink to="/" class="fw-bold list-none decoration-none font-size-none text-white hover:color-hover">
         Bobby Rahmanda
       </NuxtLink>
 
@@ -12,23 +12,23 @@
         <ul class="flex gap-2rem list-none items-center">
 
           <li>
-            <NuxtLink to="/" class="text-blue hover:text-colorTwo" active-class="text-colorTwo">Home</NuxtLink>
+            <NuxtLink to="/" class="color-font hover:color-hover" active-class="color-active">Home</NuxtLink>
           </li>
 
           <!-- <li> -->
-          <!--   <NuxtLink to="/" class="text-white hover:text-colorTwo" active-class="text-colorTwo">Blog</NuxtLink> -->
+          <!--   <NuxtLink to="/" class="text-white hover:color-hover" active-class="color-active">Blog</NuxtLink> -->
           <!-- </li> -->
 
           <li>
-            <NuxtLink to="/about" class="text-blue hover:text-colorTwo" active-class="text-colorTwo">About</NuxtLink>
+            <NuxtLink to="/about" class="color-base hover:color-hover" active-class="color-active">About</NuxtLink>
           </li>
 
           <li class="flex justify-between items-center">
             <NuxtLink to="https://github.com/bobbyrahmanda13"
-              class="mx-0.5rem text-lg text-white hover:text-colorTwo">
+              class="mx-0.5rem text-lg text-white hover:color-hover">
           <Icon name="i-bi-github" />
             </NuxtLink>
-            <NuxtLink to="https://t.me/Rahman_0000" class="mx-0.5rem text-lg text-white hover:text-colorTwo">
+            <NuxtLink to="https://t.me/Rahman_0000" class="mx-0.5rem text-lg text-white hover:color-hover">
               <Icon name="i-logos-telegram" />
             </NuxtLink>
           </li>
--- uno.config.ts
+++ uno.config.ts
@@ -14,9 +14,8 @@ export default defineConfig({
   shortcuts: {
     'border-base': 'border-gray-200 dark:border-dark-200',
     'bg-base': 'bg-white dark:bg-gray-900',
-    'color-base': 'text-gray-900 dark:text-gray-200',
-    'color-fade': 'text-gray-901:50 dark:text-gray-300:50',
     'text-hover': 'hover:text-green-600 dark:hover:text-green-400 hover:font-bold',
+    'color-font': 'text-white',
     'color-hover': 'hover:text-green-600 dark:hover:text-green-400 hover:font-bold',
     'color-active': 'text-green-600 dark:text-green-400 font-bold'
   },
