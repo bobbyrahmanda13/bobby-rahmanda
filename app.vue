43f4fdb1080b92b42a06479de12e49ef5e46dfc7
bobbyrahmanda13 <bobbyrahmanda1996@proton.me>
2024-06-09 10:24:36 +0700

    testing config nvim

--- app.vue
+++ app.vue
@@ -2,14 +2,11 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-
 </template>
 
 <script setup lang="ts">
 import '@unocss/reset/tailwind.css'
 import './styles/base.css'
-
-const jancok = ref<string>("")
 
 </script>
 
